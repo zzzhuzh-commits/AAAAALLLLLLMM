19162e7a194a2ad80a90e007043708ba2633cdfc
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 16:50:14 +0200

    Update memeifyhelpers.py

--- zlzl/helpers/memeifyhelpers.py
+++ zlzl/helpers/memeifyhelpers.py
@@ -129,6 +129,6 @@ async def silently_send_message(conv, text):
 
 
 async def thumb_from_audio(audio_path, output):
-    await _zedutils.runcmd(
-        f"ffmpeg -i {audio_path} -filter:v scale=500:500 -an {output}"
+    await runcmd(
+    f"ffmpeg -i {audio_path} -filter:v scale=500:500 -an {output}"
     )
