d9d1e5eb95457231aff9aa205542f6076764344d
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 16:44:33 +0200

    Fix import handling and add error handling for modules

--- zlzl/helpers/__init__.py
+++ zlzl/helpers/__init__.py
@@ -4,20 +4,22 @@ from .utils import *
 
 flag = True
 check = 0
+
 while flag:
     try:
-        # from .chatbot import *
-
+        from .chatbot import *
         from .functions import *
         from .memeifyhelpers import *
         from .progress import *
         from .qhelper import process
         from .tools import *
-        from .utils import _zedtools, _zedutils, _format
 
         break
+
     except ModuleNotFoundError as e:
-        install_pip(e.name)
+        pkg = "Pillow" if e.name == "PIL" else e.name
+        install_pip(pkg)
+
         check += 1
         if check > 5:
             break
